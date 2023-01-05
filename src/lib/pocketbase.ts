770311fec381193c8c657afdfce8f243f9831449
wjoseph0 <wjoseph0@proton.me>
2023-01-05 14:15:19 -0600

    new pocketbase url

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,10 +1,10 @@
-import Pocketbase from "pocketbase";
-import { writable } from "svelte/store";
+import Pocketbase from 'pocketbase';
+import { writable } from 'svelte/store';
 
-export const pb = new Pocketbase("http://172.105.0.164");
+export const pb = new Pocketbase('https://wjoseph0.cloud');
 
 export const currentUser = writable(pb.authStore.model);
 
 pb.authStore.onChange(() => {
-  currentUser.set(pb.authStore.model);
+	currentUser.set(pb.authStore.model);
 });
