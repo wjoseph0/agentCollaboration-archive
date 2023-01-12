20b9bf423e5ca0fb7232f9d99e0fda4dbc723428
wjoseph0 <wjoseph0@proton.me>
2023-01-12 10:03:51 -0600

    vite pwa initial config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { VitePWA } from 'vite-plugin-pwa'
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			devOptions: {
+				enabled: true
+			}
+		}),
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
