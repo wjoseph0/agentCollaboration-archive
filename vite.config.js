a0dc43937e48e31c74690297b2daf248e35c30af
wjoseph0 <wjoseph0@proton.me>
2023-01-12 10:10:56 -0600

    remove vite pwa

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { VitePWA } from 'vite-plugin-pwa'
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [
 		sveltekit(),
-		VitePWA({
-			registerType: 'autoUpdate',
-			devOptions: {
-				enabled: true
-			}
-		}),
 	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
