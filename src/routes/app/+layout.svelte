15ac5fc8299bc0ec260fb5c8f5fdd5d833d21054
wjoseph0 <wjoseph0@proton.me>
2023-01-17 12:59:31 -0600

    move app nav to component

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -1,17 +1,7 @@
 <script>
-	import { currentUser } from '../../lib/pocketbase';
+	import AppNav from '$lib/components/AppNav.svelte';
 </script>
 
-<nav class="container">
-	<ul>
-		<li><a href="/app">Home</a></li>
-		<li><a href="/app/chat">Chat</a></li>
-		<li><a href="/app/files">Files</a></li>
-	</ul>
-	<ul>
-		<li>Hi, {$currentUser?.fname}</li>
-		<li><a href="/app/account">Account</a></li>
-	</ul>
-</nav>
+<AppNav />
 
 <slot />
