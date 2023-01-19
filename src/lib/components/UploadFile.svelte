009be23cac0eeb302a32588c4ea00260659c9350
wjoseph0 <wjoseph0@proton.me>
2023-01-18 22:25:21 -0600

    upload file

--- src/lib/components/UploadFile.svelte
+++ src/lib/components/UploadFile.svelte
@@ -1,4 +1,43 @@
-<form>
-	<input type="file" />
-	<button>Upload</button>
-</form>
+<script>
+	import { pb, currentUser } from '$lib/pocketbase';
+
+	let showForm = false;
+	let newFile;
+	let fileName;
+
+	const uploadFile = async () => {
+		const formData = new FormData();
+		formData.append('file', newFile[0]);
+		formData.append('name', fileName);
+		formData.append('user', $currentUser.id);
+		await pb.collection('files').create(formData);
+		formVisibilty();
+	};
+
+	const formVisibilty = () => {
+		if (showForm === true) {
+			showForm = false;
+			return;
+		}
+
+		showForm = true;
+	};
+</script>
+
+{#if showForm}
+	<form on:submit|preventDefault={uploadFile}>
+		<select bind:value={fileName} required>
+			<option value="" selected>Select file type...</option>
+			<option value="Inspection Report">Inspection Report</option>
+		</select>
+		<input bind:files={newFile} type="file" required />
+		<div>
+			<button type="submit">Upload</button>
+			<button type="button" class="secondary" on:click={formVisibilty}>Cancel</button>
+		</div>
+	</form>
+{/if}
+
+{#if !showForm}
+	<button on:click={formVisibilty}>New</button>
+{/if}
