89fcff74022bb1b3514248ecd12582f3f7b3ed68
wjoseph0 <wjoseph0@proton.me>
2023-01-23 17:39:34 -0600

    FindUser component

---
+++ src/lib/components/FindUser.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { pb } from '$lib/pocketbase';
+
+	let userEmail = '';
+	let user = {};
+
+	const searchForUser = async () => {
+		try {
+			user = await pb.collection('users').getFirstListItem(`email="${userEmail}"`);
+		} catch {
+			user = { noneFound: true };
+		}
+	};
+</script>
+
+<section>
+	<input
+		type="text"
+		placeholder="Search email"
+		on:keyup={searchForUser}
+		bind:value={userEmail}
+		required
+	/>
+</section>
+
+{#if user.id}
+	<section>
+		<p>{user.fname}</p>
+		<p>{user.lname}</p>
+		<p>{user.email}</p>
+		<button>Message</button>
+	</section>
+{/if}
+
+{#if user.noneFound && userEmail}
+	<section>
+		<p>No user found.</p>
+	</section>
+{/if}
