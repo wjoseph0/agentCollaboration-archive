0ed5ac1574533de8ec4d7efb92812e92c1dfbc60
wjoseph0 <wjoseph0@proton.me>
2023-01-31 10:34:22 -0600

    buyer journey checkbox

--- src/lib/components/BuyerRoadmap.svelte
+++ src/lib/components/BuyerRoadmap.svelte
@@ -1,16 +1,52 @@
-<p>Buyer Journey</p>
+<h1>Buyer Journey</h1>
 
-<ol>
-	<li>Establish Agency Relationship</li>
-	<li>Mortgage Pre-Approval</li>
-	<li>Preview Homes</li>
-	<li>Make Offer</li>
-	<li>Write an Offer to Purchase</li>
-	<li>Accepted Offer</li>
-	<li>Earnest Money Due</li>
-	<li>Mortgage Application</li>
-	<li>Contingencies</li>
-	<li>Mortgage Approval</li>
-	<li>Final Walk-Through</li>
-	<li>Closing Day</li>
-</ol>
+<form>
+	<section>
+		<input id="1" type="checkbox" />
+		<label for="1">Establish Agency Relationship</label>
+	</section>
+	<section>
+		<input id="2" type="checkbox" />
+		<label for="2">Mortgage Pre-Approval</label>
+	</section>
+	<section>
+		<input id="3" type="checkbox" />
+		<label for="3">Preview Homes</label>
+	</section>
+	<section>
+		<input id="4" type="checkbox" />
+		<label for="4">Make Offer</label>
+	</section>
+	<section>
+		<input id="5" type="checkbox" />
+		<label for="5">Write an Offer to Purchase</label>
+	</section>
+	<section>
+		<input id="6" type="checkbox" />
+		<label for="6">Accepted Offer</label>
+	</section>
+	<section>
+		<input id="7" type="checkbox" />
+		<label for="7">Earnest Money Due</label>
+	</section>
+	<section>
+		<input id="8" type="checkbox" />
+		<label for="8">Mortgage Application</label>
+	</section>
+	<section>
+		<input id="9" type="checkbox" />
+		<label for="9">Contingencies</label>
+	</section>
+	<section>
+		<input id="10" type="checkbox" />
+		<label for="10">Mortgage Approval</label>
+	</section>
+	<section>
+		<input id="11" type="checkbox" />
+		<label for="11">Final Walk-Through</label>
+	</section>
+	<section>
+		<input id="12" type="checkbox" />
+		<label for="12">Closing Day</label>
+	</section>
+</form>
