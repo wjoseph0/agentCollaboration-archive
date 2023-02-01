23942cfc99c96d778f0213217931176d5cb170fb
wjoseph0 <wjoseph0@proton.me>
2023-02-01 09:38:43 -0600

    put styling back in page

--- src/routes/app/chat/+page.svelte
+++ src/routes/app/chat/+page.svelte
@@ -4,12 +4,39 @@
 
 <main class="container">
 	<h1>Chat</h1>
-	<Contacts />
+	<section>
+		<Contacts />
+	</section>
 </main>
 
 <style>
 	main {
 		height: 90vh;
 		display: grid;
+		overflow-y: hidden;
+	}
+
+	section {
+		overflow-y: scroll;
+	}
+
+	/* width */
+	::-webkit-scrollbar {
+		width: 1px;
+	}
+
+	/* Track */
+	::-webkit-scrollbar-track {
+		background: hsl(205deg, 16%, 77%);
+	}
+
+	/* Handle */
+	::-webkit-scrollbar-thumb {
+		background: #888;
+	}
+
+	/* Handle on hover */
+	::-webkit-scrollbar-thumb:hover {
+		background: #555;
 	}
 </style>
