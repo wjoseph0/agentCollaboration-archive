a262b3bedc8665077bc8848bab961860557e8a71
wjoseph0 <wjoseph0@proton.me>
2023-02-07 14:02:22 -0600

    protect routes

--- src/routes/files/+page.svelte
+++ src/routes/files/+page.svelte
@@ -11,7 +11,6 @@
 		</section>
 	{/if}
 	<section>
-		<h1>Files</h1>
 		<Files />
 	</section>
 </main>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -19,7 +19,7 @@
 			};
 			await pb.collection('users').create(data);
 			await pb.collection('users').authWithPassword(email, password);
-			goto('/app');
+			goto('/');
 		} catch (err) {
 			console.error(err);
 		}
@@ -33,7 +33,13 @@
 		<input placeholder="First Name" type="text" bind:value={fname} required />
 		<input placeholder="Last Name" type="text" bind:value={lname} required />
 		<input placeholder="Email" type="email" bind:value={email} required />
-		<input placeholder="Password" type="password" bind:value={password} minlength="8" required />
+		<input
+			placeholder="Password"
+			type="password"
+			bind:value={password}
+			minlength="8"
+			required
+		/>
 		<button>Signup</button>
 	</form>
 
