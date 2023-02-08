f707e44bb0bb89d31506ee0f384f93f1274d8749
wjoseph0 <wjoseph0@proton.me>
2023-02-08 16:03:21 -0600

    map page scroll fix

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -16,3 +16,30 @@
 <main class="container">
 	<BuyerRoadmap />
 </main>
+
+<style>
+	main {
+		height: 90vh;
+		overflow-y: scroll;
+	}
+
+	/* width */
+	::-webkit-scrollbar {
+		width: 1px;
+	}
+
+	/* Track */
+	::-webkit-scrollbar-track {
+		background: #11191f;
+	}
+
+	/* Handle */
+	::-webkit-scrollbar-thumb {
+		background: hsl(205deg, 16%, 77%);
+	}
+
+	/* Handle on hover */
+	::-webkit-scrollbar-thumb:hover {
+		background: rgb(185, 185, 185);
+	}
+</style>
