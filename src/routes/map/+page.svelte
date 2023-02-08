b23a7bde54a0e47ae24c9a4550564eb94d4505d2
wjoseph0 <wjoseph0@proton.me>
2023-02-08 15:39:16 -0600

    add buyer roadmap page

---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { currentUser } from '$lib/pocketbase';
+	import { goto } from '$app/navigation';
+	import { browser } from '$app/environment';
+	import BuyerRoadmap from '$lib/components/BuyerRoadmap.svelte';
+
+	$: if (
+		browser &&
+		(($currentUser.isAgent && !$currentUser.focusedClient) ||
+			(!$currentUser.isAgent && !$currentUser.agent))
+	) {
+		goto('/account');
+	}
+</script>
+
+<main class="container">
+	<BuyerRoadmap />
+</main>
