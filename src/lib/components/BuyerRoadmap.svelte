e6255f9b1f7f20a5881a9b79b0b09f3c0f00b5ba
wjoseph0 <wjoseph0@proton.me>
2023-02-14 13:54:51 -0600

    make roadmap the main page

--- src/lib/components/BuyerRoadmap.svelte
+++ src/lib/components/BuyerRoadmap.svelte
@@ -1,50 +1,46 @@
-<form>
-	<section>
-		<input id="1" type="checkbox" />
-		<label for="1">Establish Agency Relationship</label>
-	</section>
-	<section>
-		<input id="2" type="checkbox" />
-		<label for="2">Mortgage Pre-Approval</label>
-	</section>
-	<section>
-		<input id="3" type="checkbox" />
-		<label for="3">Preview Homes</label>
-	</section>
-	<section>
-		<input id="4" type="checkbox" />
-		<label for="4">Make Offer</label>
-	</section>
-	<section>
-		<input id="5" type="checkbox" />
-		<label for="5">Write an Offer to Purchase</label>
-	</section>
-	<section>
-		<input id="6" type="checkbox" />
-		<label for="6">Accepted Offer</label>
-	</section>
-	<section>
-		<input id="7" type="checkbox" />
-		<label for="7">Earnest Money Due</label>
-	</section>
-	<section>
-		<input id="8" type="checkbox" />
-		<label for="8">Mortgage Application</label>
-	</section>
-	<section>
-		<input id="9" type="checkbox" />
-		<label for="9">Contingencies</label>
-	</section>
-	<section>
-		<input id="10" type="checkbox" />
-		<label for="10">Mortgage Approval</label>
-	</section>
-	<section>
-		<input id="11" type="checkbox" />
-		<label for="11">Final Walk-Through</label>
-	</section>
-	<section>
-		<input id="12" type="checkbox" />
-		<label for="12">Closing Day</label>
-	</section>
-</form>
+<script>
+	let buyerSteps = [
+		'Establish Agency Relationship',
+		'Mortgage Pre-Approval',
+		'Preview Homes',
+		'Make Offer',
+		'Accepted Offer',
+		'Contingencies',
+		'Final Walk-Through',
+		'Closing Day'
+	];
+
+	let count = 0;
+	function counter() {
+		count++;
+		return count;
+	}
+</script>
+
+<section id="stepContainer">
+	{#each buyerSteps as step}
+		<section class="step">
+			<h3>{counter()} {step}</h3>
+		</section>
+	{/each}
+</section>
+
+<style>
+	div:nth-child(even) {
+		text-align: right;
+	}
+
+	#stepContainer {
+		background: rgb(246, 250, 223);
+		background: linear-gradient(
+			180deg,
+			rgba(246, 250, 223, 1) 0%,
+			rgba(120, 236, 108, 1) 100%
+		);
+	}
+
+	.step {
+		height: 45vh;
+		/* padding: 0 1rem 0 1rem; */
+	}
+</style>
