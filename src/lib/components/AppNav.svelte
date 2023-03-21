17703408da58bf5fef2f9db2970e909145797ff2
wjoseph0 <wjoseph0@proton.me>
2023-03-21 16:07:18 -0500

    simplify nav styling

--- src/lib/components/AppNav.svelte
+++ src/lib/components/AppNav.svelte
@@ -1,35 +1,18 @@
-<script>
-	import { currentUser } from '$lib/pocketbase';
-</script>
-
 <nav class="container">
-	<ul>
-		{#if ($currentUser.isAgent && $currentUser.focusedClient) || (!$currentUser.isAgent && $currentUser.agent)}
-			<li><a href="/"><i class="bi bi-compass-fill" /></a></li>
-			<li><a href="/chat"><i class="bi bi-chat-right-text-fill" /></a></li>
-			<li><a href="/files"><i class="bi bi-folder-fill" /></a></li>
-		{/if}
-
+	<ul class="nav">
+		<li><a href="/"><i class="bi bi-compass-fill" /></a></li>
+		<li><a href="/chat"><i class="bi bi-chat-right-text-fill" /></a></li>
+		<li><a href="/files"><i class="bi bi-folder-fill" /></a></li>
 		<li><a href="/account"><i class="bi bi-person-fill" /></a></li>
 	</ul>
 </nav>
 
 <style>
-	.container {
-		height: 0;
+	.nav {
+		display: flex;
 		width: 100%;
-	}
-
-	@media (min-width: 576px) {
-		.container {
-			height: 10vh;
-		}
-
-		ul {
-			display: flex;
-			width: 100%;
-			justify-content: space-between;
-		}
+		justify-content: space-between;
+		margin: 0;
 	}
 
 	i {
@@ -37,13 +20,7 @@
 	}
 
 	@media (max-width: 576px) {
-		ul {
-			display: flex;
-			height: 10vh;
-			width: 100%;
-			padding: inherit;
-			align-items: center;
-			justify-content: space-between;
+		.container {
 			position: fixed;
 			bottom: 0;
 		}
