df265b1ee09d96d843747bbe7b1dc74f69d88c75
wjoseph0 <wjoseph0@proton.me>
2023-03-22 10:59:28 -0500

    toggle theme

--- src/lib/components/AppNav.svelte
+++ src/lib/components/AppNav.svelte
@@ -1,9 +1,25 @@
+<script>
+	import { onMount } from 'svelte';
+	import { themeChange } from 'theme-change';
+
+	onMount(() => {
+		themeChange(false);
+	});
+</script>
+
 <nav class="container">
 	<ul class="nav">
 		<li><a href="/"><i class="bi bi-compass-fill" /></a></li>
 		<li><a href="/chat"><i class="bi bi-chat-right-text-fill" /></a></li>
 		<li><a href="/files"><i class="bi bi-folder-fill" /></a></li>
 		<li><a href="/account"><i class="bi bi-person-fill" /></a></li>
+
+		<input
+			type="checkbox"
+			class="toggle"
+			data-toggle-theme="corporate,business"
+			data-act-class="ACTIVECLASS"
+		/>
 	</ul>
 </nav>
 
