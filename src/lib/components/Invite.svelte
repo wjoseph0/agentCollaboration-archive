39e4d598bda6dbbe46038dcec5238803e24bf94f
wjoseph0 <wjoseph0@proton.me>
2023-03-27 09:32:49 -0500

    share logic

--- src/lib/components/Invite.svelte
+++ src/lib/components/Invite.svelte
@@ -1,77 +1,15 @@
 <script>
-	import Clipboard from 'clipboard';
 	import { currentUser } from '$lib/pocketbase';
 
-	const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+	const shareData = {
+		title: 'OpenAgent',
+		text: "Let's collaborate!",
+		url: `https://open-agent.vercel.app/signup/${$currentUser.id}`
+	};
 
-	const cb = new Clipboard('.copy');
-
-	let copySuccess;
-
-	cb.on('success', async () => {
-		copySuccess = true;
-		await delay(2000);
-		copySuccess = undefined;
-	});
-
-	cb.on('error', async () => {
-		copySuccess = false;
-		await delay(2000);
-		copySuccess = undefined;
-	});
-
-	let modalVisible = false;
-
-	function toggleModal() {
-		if (modalVisible === true) {
-			modalVisible = false;
-			return;
-		}
-		modalVisible = true;
-	}
+	const share = async () => {
+		await navigator.share(shareData);
+	};
 </script>
 
-{#if modalVisible}
-	<dialog open>
-		<article>
-			<!-- svelte-ignore a11y-missing-content -->
-			<a href="#top" aria-label="Close" class="close" on:click={toggleModal} />
-			<h1>Invite Client</h1>
-
-			<label
-				>Here is your sharable link!
-				<input
-					type="text"
-					placeholder="https://open-agent.vercel.app/signup/{$currentUser.id}"
-					disabled
-				/>
-			</label>
-
-			{#if copySuccess == true}
-				<button
-					data-tooltip="Copied!"
-					data-placement="top"
-					class="copy"
-					data-clipboard-text="https://open-agent.vercel.app/signup/{$currentUser.id}"
-					>Copy link</button
-				>
-			{:else if copySuccess == false}
-				<button
-					data-tooltip="Failed!"
-					data-placement="top"
-					class="copy"
-					data-clipboard-text="https://open-agent.vercel.app/signup/{$currentUser.id}"
-					>Copy link</button
-				>
-			{:else}
-				<button
-					class="copy"
-					data-clipboard-text="https://open-agent.vercel.app/signup/{$currentUser.id}"
-					>Copy link</button
-				>
-			{/if}
-		</article>
-	</dialog>
-{:else}
-	<a href="#top" role="button" on:click={toggleModal}>Invite Client</a>
-{/if}
+<a href="#top" role="button" on:click={share}>Invite Client</a>
