be30a31a3b7546af58fd386c42f0f85ca62675f8
Joseph Wagner <68037585+wjoseph0@users.noreply.github.com>
2023-03-29 13:50:55 -0500

    Calendar
    * create route
    
    * events!

---
+++ src/lib/components/Events.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let events;
+</script>
+
+{#each events as event}
+	<div>
+		<input type="date" value={event.date.substring(0, 10)} disabled />
+		<p>{event.name}</p>
+	</div>
+{/each}
+
+<style>
+	div {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
