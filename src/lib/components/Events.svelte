b5e2f4973cac459fbf60ff4ffaa67c18262c957b
wjoseph0 <wjoseph0@proton.me>
2023-03-31 11:11:38 -0500

    events layout

--- src/lib/components/Events.svelte
+++ src/lib/components/Events.svelte
@@ -2,18 +2,32 @@
 	export let events;
 </script>
 
-{#each events as event}
-	<div>
-		<input type="date" value={event.date.substring(0, 10)} disabled />
-		<p>{event.name}</p>
-	</div>
-{/each}
+<table>
+	<thead>
+		<tr>
+			<th scope="col" />
+			<th scope="col" />
+			<th scope="col" />
+		</tr>
+	</thead>
+	<tbody>
+		{#each events as event}
+			<tr>
+				<th scope="row"><input type="date" value={event.date.substring(0, 10)} disabled /></th>
+				<td id="title">{event.name}</td>
+				<td><a href="#top">Edit</a></td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
 
 <style>
-	div {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+	input {
+		width: auto;
+	}
+
+	#title {
+		font-size: large;
+		font-weight: 600;
 	}
 </style>
