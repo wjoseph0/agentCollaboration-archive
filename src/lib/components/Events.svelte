ff61dd80761d4c7f4591225e223f1b7686644a00
Joseph Wagner <68037585+wjoseph0@users.noreply.github.com>
2023-03-31 13:15:22 -0500

    Calendar C.R.U.D.
    * handle realtime connection
    
    * events layout
    
    * edit and delete event

--- src/lib/components/Events.svelte
+++ src/lib/components/Events.svelte
@@ -1,19 +1,36 @@
 <script>
+	import EditEvent from '$lib/components/EditEvent.svelte';
 	export let events;
 </script>
 
-{#each events as event}
-	<div>
-		<input type="date" value={event.date.substring(0, 10)} disabled />
-		<p>{event.name}</p>
-	</div>
-{/each}
+<table>
+	<thead>
+		<tr>
+			<th scope="col" />
+			<th scope="col" />
+			<th scope="col" />
+		</tr>
+	</thead>
+	<tbody>
+		{#each events as event}
+			<tr>
+				<th scope="row"><input type="date" value={event.date.substring(0, 10)} disabled /></th>
+				<td id="title">{event.name}</td>
+				<td>
+					<EditEvent recordId={event.id} name={event.name} date={event.date.substring(0, 10)} />
+				</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
 
 <style>
-	div {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+	input {
+		width: auto;
+	}
+
+	#title {
+		font-size: large;
+		font-weight: 600;
 	}
 </style>
--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	import { pb, currentUser } from '$lib/pocketbase';
-	import { onMount } from 'svelte';
+	import { onMount, onDestroy } from 'svelte';
 	import { goto } from '$app/navigation';
 	import { browser } from '$app/environment';
 	import ChooseClient from '$lib/components/ChooseClient.svelte';
@@ -14,12 +14,33 @@
 
 	let events = [];
 
+	const sortFunction = (a, b) => {
+		const date1 = new Date(a.date);
+		const date2 = new Date(b.date);
+		return date1 - date2;
+	};
+
 	onMount(async () => {
 		await pb.collection('users').authRefresh({}, { expand: 'agent,clients,focusedClient' });
+
 		events = await pb.collection('events').getFullList(200, { sort: '+date' });
-		await pb.collection('events').subscribe('*', async () => {
-			events = await pb.collection('events').getFullList(200, { sort: '+date' });
+
+		pb.collection('events').subscribe('*', async ({ action, record }) => {
+			if (action === 'create') {
+				events.push(record);
+				events = events.sort(sortFunction);
+			} else if (action === 'update') {
+				const i = events.findIndex((e) => e.id === record.id);
+				events.splice(i, 1, record);
+				events = events.sort(sortFunction);
+			} else if (action === 'delete') {
+				events = events.filter((e) => e.id !== record.id);
+			}
 		});
+	});
+
+	onDestroy(async () => {
+		pb.collection('events').unsubscribe();
 	});
 </script>
 
