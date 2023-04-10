a033e855b2bf3c87a6215b16661a63e9ccbd9801
wjoseph0 <wjoseph0@proton.me>
2023-04-10 10:48:52 -0500

    app nav separator

--- src/lib/components/AppNav.svelte
+++ src/lib/components/AppNav.svelte
@@ -2,7 +2,7 @@
 	<ul class="nav">
 		<li><a href="/calendar"><i class="bi bi-calendar-event" /> </a></li>
 		<li><a href="/chat"><i class="bi bi-chat" /> </a></li>
-		<li><a href="/"><i class="bi bi-geo-alt" /></a></li>
+		<li><a href="/"><i class="bi bi-house" /></a></li>
 		<li><a href="/files"><i class="bi bi-folder" /></a></li>
 		<li><a href="/account"><i class="bi bi-person" /></a></li>
 	</ul>
@@ -16,14 +16,29 @@
 		margin: 0;
 	}
 
+	.container {
+		border-bottom: 1px solid #ededed;
+	}
+
 	i {
 		font-size: 2em;
+	}
+
+	a,
+	li {
+		padding: 0;
+		margin: 0;
 	}
 
 	@media (max-width: 576px) {
 		.container {
 			position: fixed;
 			bottom: 0;
+			border-top: 1px solid #ededed;
+		}
+
+		.nav {
+			padding-bottom: 1.375em;
 		}
 	}
 </style>
