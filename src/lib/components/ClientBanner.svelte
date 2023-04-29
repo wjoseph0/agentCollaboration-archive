4e55e32b9992af9b55f8d52d560a31c03a8e1e81
wjoseph0 <wjoseph0@proton.me>
2023-04-29 14:21:07 -0500

    Remove agent banner

--- src/lib/components/ClientBanner.svelte
+++ src/lib/components/ClientBanner.svelte
@@ -27,26 +27,7 @@
 		</h2>
 		<Clients />
 	</div>
-{:else if !$currentUser.isAgent && $currentUser.agent}
-	<h2>
-		{#if $currentUser.expand.agent.profilePic}
-			<img
-				src="https://wjoseph0.cloud/api/files/_pb_users_auth_/{$currentUser.agent}/{$currentUser
-					.expand.agent.profilePic}"
-				alt="profile pic"
-				width="40px"
-			/>
-		{:else}
-			<img
-				class="avatar"
-				src={`https://api.dicebear.com/5.x/initials/svg?seed=${$currentUser.expand.agent.fname}%20${$currentUser.expand.agent.lname}`}
-				alt="avatar"
-				width="40px"
-			/>
-		{/if}
-		{$currentUser.expand.agent.fname}
-		{$currentUser.expand.agent.lname}
-	</h2>
+
 {/if}
 
 <style>
