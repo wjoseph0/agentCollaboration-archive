82df25b8440779bcc8bb684f04ad146fa2e78921
wjoseph0 <wjoseph0@proton.me>
2023-06-09 12:17:11 -0500

    settings page buttons

--- src/lib/components/AccountType.svelte
+++ src/lib/components/AccountType.svelte
@@ -1,16 +1,6 @@
 <script>
 	import { pb, currentUser } from '$lib/pocketbase';
 
-	let modalVisible = false;
-
-	function toggleModal() {
-		if (modalVisible === true) {
-			modalVisible = false;
-			return;
-		}
-		modalVisible = true;
-	}
-
 	async function switchAccountType() {
 		let data;
 		if ($currentUser.isAgent) {
@@ -27,35 +17,26 @@
 			expand: 'agent,clients,focusedClient'
 		});
 
-		toggleModal();
-
 		await pb.collection('users').authRefresh({}, { expand: 'agent,clients,focusedClient' });
 	}
 </script>
 
-{#if modalVisible}
-	<dialog open>
-		<article>
-			<!-- svelte-ignore a11y-missing-content -->
-			<a
-				href="/account"
-				aria-label="Close"
-				class="close"
-				data-target="modal-example"
-				on:click={toggleModal}
-			/>
-			<h3>Confirm change!</h3>
-			{#if $currentUser.isAgent}
-				<p>Your account type will revert back to a <strong>client</strong>.</p>
-			{:else}
-				<p>Your account type will change to an <strong>agent</strong>.</p>
-			{/if}
-			<footer>
-				<a href="/account" role="button" class="secondary" on:click={toggleModal}> Cancel </a>
-				<a href="/account" role="button" on:click={switchAccountType}> Confirm </a>
-			</footer>
-		</article>
-	</dialog>
-{:else}
-	<button class="outline" on:click={toggleModal}>Change Account Type</button>
-{/if}
+<!-- Open the modal using ID.showModal() method -->
+<button class="btn btn-outline btn-secondary" onclick="my_modal_5.showModal()"
+	>Change Account Type</button
+>
+<dialog id="my_modal_5" class="modal modal-bottom sm:modal-middle">
+	<form method="dialog" class="modal-box">
+		<h3 class="font-bold text-lg">Confirm Change</h3>
+		{#if $currentUser.isAgent}
+			<p class="py-4">Your account type will revert back to a <strong>client</strong>.</p>
+		{:else}
+			<p class="py-4">Your account type will change to an <strong>agent</strong>.</p>
+		{/if}
+		<div class="modal-action">
+			<!-- if there is a button in form, it will close the modal -->
+			<button class="btn">Cancel</button>
+			<button class="btn btn-outline btn-secondary" on:click={switchAccountType}> Confirm </button>
+		</div>
+	</form>
+</dialog>
