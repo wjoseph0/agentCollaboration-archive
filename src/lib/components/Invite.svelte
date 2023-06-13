59f2546bfb37eb742c6eac67b3fbaf9d7f5d0cea
wjoseph0 <wjoseph0@proton.me>
2023-06-13 11:49:18 -0500

    invite button

--- src/lib/components/Invite.svelte
+++ src/lib/components/Invite.svelte
@@ -4,7 +4,7 @@
 	const shareData = {
 		title: 'Agentpad',
 		text: "Hi! Let's use Agentpad to collaborate!",
-		url: `https://agentpadapp.com/signup/${$currentUser.id}`
+		url: `https://app.agentpadapp.com/signup/${$currentUser.id}`
 	};
 
 	const share = async () => {
@@ -12,4 +12,4 @@
 	};
 </script>
 
-<a href="#top" role="button" on:click={share}>Invite</a>
+<button class="btn" on:click={share}>Invite</button>
