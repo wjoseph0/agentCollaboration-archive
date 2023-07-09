8faf7164011eac36848ea3831e5c47c35249ee33
wjoseph0 <wjoseph0@proton.me>
2023-07-09 11:48:25 -0500

    client roadmap

--- src/lib/components/BuyerRoadmap.svelte
+++ src/lib/components/BuyerRoadmap.svelte
@@ -7,13 +7,16 @@
 	import preparationImg from '$lib/assets/preparation-img.jpg';
 	import searchingImg from '$lib/assets/searching-img.jpg';
 	import closingImg from '$lib/assets/closing-img.jpg';
+	import closedImg from '$lib/assets/closed-img.jpg';
 
 	let journey;
 	let noJourney;
 
 	onMount(async () => {
 		try {
-			journey = await pb.collection('journeys').getList(1, 1, {});
+			journey = await pb
+				.collection('journeys')
+				.getFirstListItem(`client='${$currentUser.id}' && agent='${$currentUser.agent}'`);
 
 			// Subscribe to changes only in the specified record
 			await pb.collection('journeys').subscribe(journey.id, async (e) => {
@@ -43,10 +46,10 @@
 			<h1>Your Journey</h1>
 		</div>
 		<br />
-		<section class="flex justify-center">
+		<div class="flex justify-center">
 			<ul class="steps steps-vertical">
 				{#each stages as stage}
-					<li class="step w-full">
+					<li class={stage.step <= journey.step ? 'step step-primary w-full' : 'step w-full'}>
 						<div class="card w-auto bg-base-100 shadow-xl">
 							<figure>
 								{#if stage.step === 1}
@@ -55,12 +58,13 @@
 									<img src={searchingImg} alt="" />
 								{:else if stage.step === 3}
 									<img src={closingImg} alt="" />
+								{:else if stage.step === 4}
+									<img src={closedImg} alt="" />
 								{/if}
 							</figure>
 							<div class="card-body h-50">
 								<h2 class="card-title">{stage.name}</h2>
-
-								<div class="card-actions flex">
+								<div class="card-actions">
 									{#if stage.step === 2}
 										<SearchProfile {journey} />
 										<OfferCheatSheet {journey} />
@@ -71,7 +75,10 @@
 					</li>
 				{/each}
 			</ul>
-		</section>
+		</div>
+		<br />
+		<br />
+		<br />
 	</div>
 {:else if noJourney}
 	<div class="h-screen flex justify-center items-center">
