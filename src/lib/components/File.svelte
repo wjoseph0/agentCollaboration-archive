2b141917915a36e4cd3580a42541e3de218c3e50
wjoseph0 <wjoseph0@proton.me>
2023-07-12 17:40:58 -0500

    file visibility

--- src/lib/components/File.svelte
+++ src/lib/components/File.svelte
@@ -1,16 +1,15 @@
 <script>
-	import { pb } from '$lib/pocketbase';
-	import OfferCheatSheet from '$lib/components/OfferCheatSheet.svelte';
-	import SearchProfile from '$lib/components/SearchProfile.svelte';
-	import Files from './Files.svelte';
+	import { pb, currentUser } from '$lib/pocketbase';
 
 	export let file;
 
 	let fileName = file.name;
+	let isPublic = file.isPublic;
 
-	const updateFileName = async () => {
+	const updateFile = async () => {
 		const data = {
-			name: fileName
+			name: fileName,
+			isPublic: isPublic
 		};
 
 		await pb.collection('files').update(file.id, data);
@@ -20,15 +19,68 @@
 <!-- Open the modal using ID.showModal() method -->
 <dialog id="c{file.id}" class="modal">
 	<!-- svelte-ignore a11y-click-events-have-key-events -->
-	<form method="dialog" class="modal-box prose" on:click={updateFileName}>
+	<form method="dialog" class="modal-box prose" on:click={updateFile}>
 		<button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
 		<!-- svelte-ignore a11y-click-events-have-key-events -->
 
-		<form on:submit|preventDefault={updateFileName}>
+		<form on:submit|preventDefault={updateFile}>
 			<input type="text" bind:value={fileName} class="input input-ghost w-full font-bold" />
 		</form>
 		<br />
-		<div class="modal-action">
+		<div class="modal-action flex flex-row justify-between items-center">
+			{#if $currentUser.isAgent}
+				{#if file.isPublic}
+					<div
+						class="flex flex-row justify-end items-center cursor-pointer"
+						on:click={() => {
+							isPublic = false;
+							updateFile();
+						}}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							class="w-5 h-5"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M14.5 1A4.5 4.5 0 0010 5.5V9H3a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-1.5V5.5a3 3 0 116 0v2.75a.75.75 0 001.5 0V5.5A4.5 4.5 0 0014.5 1z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</div>
+				{:else if !file.isPublic && !file.client}
+					<div
+						class="flex flex-row items-center cursor-pointer"
+						on:click={() => {
+							isPublic = true;
+							updateFile();
+						}}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							class="w-5 h-5"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</div>
+				{:else if file.client}
+					<div class="flex flex-row justify-end items-center">
+						<div class="avatar placeholder">
+							<div class="bg-neutral-focus text-neutral-content rounded-full w-8">
+								<span class="text-xs" />
+							</div>
+						</div>
+					</div>
+				{/if}
+			{/if}
 			<a
 				target="_blank"
 				rel="noreferrer"
@@ -39,7 +91,7 @@
 		</div>
 	</form>
 	<!-- svelte-ignore a11y-click-events-have-key-events -->
-	<form method="dialog" class="modal-backdrop" on:click={updateFileName}>
+	<form method="dialog" class="modal-backdrop" on:click={updateFile}>
 		<button>close</button>
 	</form>
 </dialog>
--- src/lib/components/Files.svelte
+++ src/lib/components/Files.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { currentUser } from '$lib/pocketbase';
 	import { page } from '$app/stores';
 	import File from '$lib/components/File.svelte';
 
@@ -28,12 +29,58 @@
 							{file.name}
 						</div>
 					</td>
+					<td>
+						{#if $currentUser.isAgent}
+							{#if file.isPublic}
+								<div class="flex flex-row justify-end items-center">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 20 20"
+										fill="currentColor"
+										class="w-5 h-5"
+									>
+										<path
+											fill-rule="evenodd"
+											d="M14.5 1A4.5 4.5 0 0010 5.5V9H3a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-1.5V5.5a3 3 0 116 0v2.75a.75.75 0 001.5 0V5.5A4.5 4.5 0 0014.5 1z"
+											clip-rule="evenodd"
+										/>
+									</svg>
+
+									<span>Public</span>
+								</div>
+							{:else if !file.isPublic && !file.client}
+								<div class="flex flex-row justify-end items-center">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 20 20"
+										fill="currentColor"
+										class="w-5 h-5"
+									>
+										<path
+											fill-rule="evenodd"
+											d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
+											clip-rule="evenodd"
+										/>
+									</svg>
+									<span>Private</span>
+								</div>
+							{:else if file.client}
+								<div class="flex flex-row justify-end items-center">
+									<div class="avatar placeholder">
+										<div class="bg-neutral-focus text-neutral-content rounded-full w-8">
+											<span class="text-xs" />
+										</div>
+									</div>
+								</div>
+							{/if}
+						{/if}
+					</td>
 				</tr>
 				<File {file} />
 			{/each}
 		</tbody>
 	</table>
-{:else}
+{:else if path === '/'}
 	<table class="table">
 		<tbody>
 			{#each files as file (file.id)}
