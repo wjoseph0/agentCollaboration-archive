abc54b8e73e38fc6e6f0e61def50d8c58a87412f
wjoseph0 <wjoseph0@proton.me>
2023-07-25 15:33:33 -0500

    support page

---
+++ src/routes/support/+page.svelte
@@ -0,0 +1,12 @@
+<main class="container prose flex flex-col justify-center h-screen">
+	<h1 class="text-center text-neutral">Agentpad Support</h1>
+	<div class="card w-auto bg-base-100 shadow-2xl">
+		<div class="card-body">
+			<h2 class="card-title">support@agentpadapp.com</h2>
+			<p class="text-right text-xs">
+				Response time for customers: <span class="text-success">~2 hours</span> <br />
+				Other inquiries: <span class="text-secondary">~24 hours</span>
+			</p>
+		</div>
+	</div>
+</main>
