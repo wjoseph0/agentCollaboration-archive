2d576d58c44570dfa7318fd9e0978723529d521c
wjoseph0 <wjoseph0@proton.me>
2023-08-11 10:05:29 -0500

    test original

--- src/lib/components/Invite.svelte
+++ src/lib/components/Invite.svelte
@@ -3,12 +3,17 @@
 
 	const shareData = {
 		title: 'Agentpad',
-		text: "Hi! Let's use Agentpad to collaborate!Please download the app from your app store:Apple App Store: https://apps.apple.com/us/app/agentpad/id6453560305Google Play Store: https://play.google.com/store/apps/details?id=com.agentpadapp.twaAfter downloading, please sign up and enter my ID. My ID is ${$currentUser.id}"
+		text: "Hi! Let's use Agentpad to collaborate!",
+		url: 'https://agentpadapp.com/signup'
 	};
 
+	// "Hi! Let's use Agentpad to collaborate!Please download the app from your app store:Apple App Store: https://apps.apple.com/us/app/agentpad/id6453560305Google Play Store: https://play.google.com/store/apps/details?id=com.agentpadapp.twaAfter downloading, please sign up and enter my ID. My ID is ${$currentUser.id}"
 	const share = async () => {
-		await navigator.share(shareData);
-		console.log(shareData);
+		try {
+			await navigator.share(shareData);
+		} catch (error) {
+			console.log(error);
+		}
 	};
 </script>
 
