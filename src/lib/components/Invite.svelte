c81cc25f4d9718d258e0fdcf4e4c2ea25e6b6718
wjoseph0 <wjoseph0@proton.me>
2023-08-11 09:42:00 -0500

    test

--- src/lib/components/Invite.svelte
+++ src/lib/components/Invite.svelte
@@ -3,13 +3,7 @@
 
 	const shareData = {
 		title: 'Agentpad',
-		text: `Hi! Let's use Agentpad to collaborate!
-Please download the app from your app store:
-Apple App Store: https://apps.apple.com/us/app/agentpad/id6453560305
-
-Google Play Store: https://play.google.com/store/apps/details?id=com.agentpadapp.twa
-
-After downloading, please sign up and enter my ID. My ID is ${$currentUser.id}`
+		text: "Hi! Let's use Agentpad to collaborate!Please download the app from your app store:Apple App Store: https://apps.apple.com/us/app/agentpad/id6453560305Google Play Store: https://play.google.com/store/apps/details?id=com.agentpadapp.twaAfter downloading, please sign up and enter my ID. My ID is ${$currentUser.id}"
 	};
 
 	const share = async () => {
