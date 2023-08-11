91a34a6e53927724a438a7beddf267da09f0680e
wjoseph0 <wjoseph0@proton.me>
2023-08-11 10:41:50 -0500

    text format

--- src/lib/components/Invite.svelte
+++ src/lib/components/Invite.svelte
@@ -2,10 +2,14 @@
 	import { currentUser } from '$lib/pocketbase';
 
 	const shareText = `Hi! Let's use Agentpad to collaborate!
+
 Please download the app from your app store:
-Apple App Store: https://apps.apple.com/us/app/agentpad/id6453560305Google
-Play Store: https://play.google.com/store/apps/details?id=com.agentpadapp.twa
-After downloading, please sign up and enter my ID. My ID is ${$currentUser.id}`;
+
+Apple App Store: https://apps.apple.com/us/app/agentpad/id6453560305
+Google Play Store: https://play.google.com/store/apps/details?id=com.agentpadapp.twa
+
+After downloading, please sign up and enter my ID. 
+My ID is ${$currentUser.id}`;
 
 	const data = {
 		title: 'Agentpad',
