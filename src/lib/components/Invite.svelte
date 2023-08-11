2f632d8ddd7183c033e49c9e3f9a1a67f60e0df0
wjoseph0 <wjoseph0@proton.me>
2023-08-11 10:44:57 -0500

    format again

--- src/lib/components/Invite.svelte
+++ src/lib/components/Invite.svelte
@@ -6,9 +6,11 @@
 Please download the app from your app store:
 
 Apple App Store: https://apps.apple.com/us/app/agentpad/id6453560305
+
 Google Play Store: https://play.google.com/store/apps/details?id=com.agentpadapp.twa
 
-After downloading, please sign up and enter my ID. 
+After downloading, please sign up and enter my ID.
+
 My ID is ${$currentUser.id}`;
 
 	const data = {
