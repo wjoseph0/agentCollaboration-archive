0cf8f6d510b4892e103d6b516d7fa43a8321c7ff
wjoseph0 <wjoseph0@proton.me>
2023-08-11 09:23:17 -0500

    first revision

--- src/lib/components/Invite.svelte
+++ src/lib/components/Invite.svelte
@@ -3,7 +3,14 @@
 
 	const shareData = {
 		title: 'Agentpad',
-		text: "Hi! Let's use Agentpad to collaborate!",
+		text: `Hi! Let's use Agentpad to collaborate!
+
+				Please download the app from your app store:
+				Apple App Store: https://apps.apple.com/us/app/agentpad/id6453560305
+				
+				Google Play Store: https://play.google.com/store/apps/details?id=com.agentpadapp.twa
+				
+				After downloading, please sign up and enter my ID. My ID is ${$currentUser.id}`,
 		url: `https://agentpadapp.com/signup/${$currentUser.id}`
 	};
 
