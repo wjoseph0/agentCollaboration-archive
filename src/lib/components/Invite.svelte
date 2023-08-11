4edc988936c40cc6ea1f2596d5b4f0850c581177
wjoseph0 <wjoseph0@proton.me>
2023-08-11 10:23:38 -0500

    r

--- src/lib/components/Invite.svelte
+++ src/lib/components/Invite.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { currentUser } from '$lib/pocketbase';
 
-	const shareData = {
+	const data = {
 		title: 'Agentpad',
 		text: "Hi! Let's use Agentpad to collaborate!",
 		url: 'https://agentpadapp.com/signup'
@@ -9,11 +9,7 @@
 
 	// "Hi! Let's use Agentpad to collaborate!Please download the app from your app store:Apple App Store: https://apps.apple.com/us/app/agentpad/id6453560305Google Play Store: https://play.google.com/store/apps/details?id=com.agentpadapp.twaAfter downloading, please sign up and enter my ID. My ID is ${$currentUser.id}"
 	const share = async () => {
-		try {
-			await navigator.share(shareData);
-		} catch (error) {
-			console.log(error);
-		}
+		await navigator.share(data);
 	};
 </script>
 
