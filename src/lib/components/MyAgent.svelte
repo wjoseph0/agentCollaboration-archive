c9c1964cbdb2337c478940ddd900242d2fb97c05
wjoseph0 <wjoseph0@proton.me>
2023-08-30 19:56:53 -0500

    contact info

--- src/lib/components/MyAgent.svelte
+++ src/lib/components/MyAgent.svelte
@@ -32,7 +32,48 @@
 						{$currentUser.expand.agent.fname}
 						{$currentUser.expand.agent.lname}
 					</h2>
-					<p class="text-sm">{$currentUser.expand.agent.email}</p>
+					<div class="flex flex-col justify-center items-start gap-4 py-4">
+						<div class="flex flex-row justify-center items-center gap-1">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="w-5 h-5"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+								/>
+							</svg>
+							{#if $currentUser.expand.agent.contact_email}
+								<span class="text-sm">{$currentUser.expand.agent.contact_email}</span>
+							{:else}
+								<span class="text-sm">{$currentUser.expand.agent.email}</span>
+							{/if}
+						</div>
+						{#if $currentUser.expand.agent.contact_number}
+							<div class="flex flex-row justify-center items-center gap-1">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="w-5 h-5"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+									/>
+								</svg>
+								<span class="text-sm">{$currentUser.expand.agent.contact_number}</span>
+							</div>
+						{/if}
+					</div>
 				</div>
 			</div>
 			{#if $currentUser.expand.agent.teamLogo && $currentUser.expand.agent.brokerageLogo}
