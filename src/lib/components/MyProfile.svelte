fab8561ed781e1b892059d788c9e00e5ee6a3b3a
wjoseph0 <wjoseph0@proton.me>
2023-09-10 12:46:41 -0500

    Edit contact info

--- src/lib/components/MyProfile.svelte
+++ src/lib/components/MyProfile.svelte
@@ -3,6 +3,8 @@
 
 	let contact_email_input;
 	let contact_number_input;
+	let loading = false;
+	let success = false;
 
 	const setContactInfo = async () => {
 		const data = {
@@ -10,7 +12,17 @@
 			contact_number: contact_number_input.value
 		};
 
-		await pb.collection('users').update($currentUser.id, data);
+		try {
+			loading = true;
+			await pb.collection('users').update($currentUser.id, data);
+			loading = false;
+			success = true;
+			setTimeout(() => {
+				success = false;
+			}, 1000);
+		} catch (error) {
+			console.error(error);
+		}
 	};
 </script>
 
@@ -125,73 +137,98 @@
 								<span>{$currentUser.contact_number}</span>
 							</div>
 						</div>
+						<div class="flex justify-center items-center">
+							<button onclick="contactInfo.showModal()" class="btn btn-primary btn-xs">Edit</button>
+						</div>
 					</div>
 				{:else}
 					<button class="btn btn-primary btn-sm mx-auto" onclick="contactInfo.showModal()"
 						>Add Contact Information</button
 					>
-
-					<dialog id="contactInfo" class="modal">
-						<form method="dialog" class="modal-box">
-							<h3 class="font-bold text-lg">Contact Information</h3>
-
-							<div class="flex flex-row justify-start items-center gap-2 py-6">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="1.5"
-									stroke="currentColor"
-									class="w-6 h-6"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
-									/>
-								</svg>
-								<input
-									bind:this={contact_email_input}
-									type="text"
-									placeholder="Email"
-									class="input input-bordered w-full max-w-xs"
-								/>
-							</div>
-
-							<div class="flex flex-row justify-start items-center gap-2">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="1.5"
-									stroke="currentColor"
-									class="w-6 h-6"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
-									/>
-								</svg>
-								<input
-									bind:this={contact_number_input}
-									type="text"
-									placeholder="Phone Number"
-									class="input input-bordered w-full max-w-xs"
-								/>
-							</div>
-
-							<div class="modal-action">
-								<!-- if there is a button, it will close the modal -->
-								<!-- svelte-ignore a11y-click-events-have-key-events -->
-								<span class="btn" on:click={setContactInfo}>Save</span>
-							</div>
-						</form>
-						<form method="dialog" class="modal-backdrop">
-							<button>close</button>
-						</form>
-					</dialog>
 				{/if}
+
+				<dialog id="contactInfo" class="modal">
+					<form method="dialog" class="modal-box">
+						<h3 class="font-bold text-lg">Contact Information</h3>
+
+						<div class="flex flex-row justify-start items-center gap-2 py-6">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="w-6 h-6"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+								/>
+							</svg>
+							<input
+								bind:this={contact_email_input}
+								value={$currentUser.contact_email}
+								type="text"
+								placeholder="Email"
+								class="input input-bordered w-full max-w-xs"
+							/>
+						</div>
+
+						<div class="flex flex-row justify-start items-center gap-2">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="w-6 h-6"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+								/>
+							</svg>
+							<input
+								bind:this={contact_number_input}
+								value={$currentUser.contact_number}
+								type="text"
+								placeholder="Phone Number"
+								class="input input-bordered w-full max-w-xs"
+							/>
+						</div>
+
+						<div class="modal-action">
+							<!-- if there is a button, it will close the modal -->
+							<!-- svelte-ignore a11y-click-events-have-key-events -->
+
+							{#if success}
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="w-6 h-6 text-success"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+									/>
+								</svg>
+							{:else if loading}
+								<span class="loading loading-spinner loading-xs" />
+							{:else}
+								<span class="btn" on:click={setContactInfo}>Save </span>
+							{/if}
+						</div>
+					</form>
+					<form method="dialog" class="modal-backdrop">
+						<button>close</button>
+					</form>
+				</dialog>
 			</div>
 		</div>
 	{/if}
