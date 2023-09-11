962c8bf3ef8fa91443c7f47cbd0a9f7131e0a1cc
wjoseph0 <wjoseph0@proton.me>
2023-09-11 12:48:35 -0500

    Start journey more clear

--- src/lib/components/StartJourney.svelte
+++ src/lib/components/StartJourney.svelte
@@ -24,4 +24,23 @@
 	};
 </script>
 
-<button on:click={createJourney} class="btn btn-primary w-full max-w-md">Start Journey</button>
+<div class="flex flex-col justify-center items-center gap-3">
+	<h2>Press the button below to start your journey.</h2>
+
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="w-6 h-6"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3"
+		/>
+	</svg>
+
+	<button on:click={createJourney} class="btn btn-primary w-full max-w-md">Start Journey</button>
+</div>
