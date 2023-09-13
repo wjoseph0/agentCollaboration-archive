88e3454df7b832ac858ae7d8d517b63664fc3f5a
wjoseph0 <wjoseph0@proton.me>
2023-09-13 10:49:10 -0500

    Remove file avatar

--- src/lib/components/File.svelte
+++ src/lib/components/File.svelte
@@ -85,27 +85,7 @@
 					</div>
 				{/if}
 			{:else if !$currentUser.isAgent}
-				{#if file.owner === $currentUser.id}
-					<div class="flex flex-row justify-end items-center">
-						<div class="avatar placeholder">
-							<div class="bg-neutral-focus text-neutral-content rounded-full w-8">
-								<!-- <span class="text-xs">{$currentUser.fname[0]}{$currentUser.lname[0]}</span> -->
-								<span class="text-xs"
-									><svg
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 20 20"
-										fill="currentColor"
-										class="w-5 h-5"
-									>
-										<path
-											d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z"
-										/>
-									</svg>
-								</span>
-							</div>
-						</div>
-					</div>
-				{/if}
+				<div />
 			{/if}
 			<a
 				target="_blank"
