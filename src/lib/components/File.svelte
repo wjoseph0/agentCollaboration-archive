474d0023fc5d61f51ea9d7ee7f6c521b560d895d
wjoseph0 <wjoseph0@proton.me>
2023-09-15 12:59:31 -0500

    Allow agent to delete their files

--- src/lib/components/File.svelte
+++ src/lib/components/File.svelte
@@ -32,6 +32,10 @@
 			fileLinkElement.click();
 		}, 0);
 	};
+
+	const deleteFile = async () => {
+		await pb.collection('files').delete(file.id);
+	};
 </script>
 
 <!-- Open the modal using ID.showModal() method -->
@@ -47,97 +51,86 @@
 		</form>
 		<br />
 		<div class="modal-action flex flex-row justify-between items-center">
-			{#if $currentUser.isAgent}
-				{#if file.isPublic}
-					<!-- svelte-ignore a11y-no-static-element-interactions -->
-					<div
-						class="flex flex-row justify-end items-center cursor-pointer"
-						on:click={() => {
-							isPublic = false;
-							clientsOnly = true;
-							updateFile();
-						}}
+			{#if $currentUser.isAgent && (file.isPublic || file.clientsOnly)}
+				<!-- svelte-ignore a11y-no-static-element-interactions -->
+				<div class="hover:brightness-75" on:click={deleteFile}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-5 h-5 text-error"
 					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 20 20"
-							fill="currentColor"
-							class="w-5 h-5"
-						>
-							<path
-								fill-rule="evenodd"
-								d="M14.5 1A4.5 4.5 0 0010 5.5V9H3a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-1.5V5.5a3 3 0 116 0v2.75a.75.75 0 001.5 0V5.5A4.5 4.5 0 0014.5 1z"
-								clip-rule="evenodd"
-							/>
-						</svg>
-					</div>
-				{:else if !file.isPublic && !file.client}
-					<!-- svelte-ignore a11y-no-static-element-interactions -->
-					<div
-						class="flex flex-row items-center cursor-pointer"
-						on:click={() => {
-							isPublic = true;
-							clientsOnly = false;
-							updateFile();
-						}}
-					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 20 20"
-							fill="currentColor"
-							class="w-5 h-5"
-						>
-							<path
-								fill-rule="evenodd"
-								d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
-								clip-rule="evenodd"
-							/>
-						</svg>
-					</div>
-				{:else if file.client}
-					<div class="flex flex-row justify-end items-center">
-						<div class="avatar placeholder">
-							<div class="bg-neutral-focus text-neutral-content rounded-full w-8">
-								<span class="text-xs" />
-							</div>
-						</div>
-					</div>
-				{/if}
-			{:else if !$currentUser.isAgent}
-				{#if !file.client}
-					<div class="ml-3">
-						{#if $currentUser.expand.agent.profilePic}
-							<!-- <div class="avatar">
-								<div class="w-8 rounded-full">
-									<img
-										src="https://wjoseph0.cloud/api/files/_pb_users_auth_/{$currentUser.agent}/{$currentUser
-											.expand.agent.profilePic}"
-										alt=""
-									/>
-								</div>
-							</div> -->
-							<div />
-						{:else}
-							<div class="avatar placeholder">
-								<div class="rounded-full bg-neutral-focus text-neutral-content w-8">
-									<span class="text-xs">
-										{$currentUser.expand.agent.fname[0]}{$currentUser.expand.agent.lname[0]}
-									</span>
-								</div>
-							</div>
-						{/if}
-					</div>
-				{:else}
-					<div />
-				{/if}
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+						/>
+					</svg>
+				</div>
+			{:else}
+				<div />
 			{/if}
 
-			{#if loading}
-				<span class="btn"><span class="loading loading-spinner loading-xs"></span></span>
-			{:else}
-				<!-- svelte-ignore a11y-no-static-element-interactions -->
-				<span class="btn" on:click={viewFile}>View</span>
-			{/if}
+			<div class="flex gap-4">
+				{#if $currentUser.isAgent}
+					{#if file.isPublic}
+						<!-- svelte-ignore a11y-no-static-element-interactions -->
+						<div
+							class="flex flex-row justify-end items-center cursor-pointer"
+							on:click={() => {
+								isPublic = false;
+								clientsOnly = true;
+								updateFile();
+							}}
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								class="w-5 h-5"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M14.5 1A4.5 4.5 0 0010 5.5V9H3a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-1.5V5.5a3 3 0 116 0v2.75a.75.75 0 001.5 0V5.5A4.5 4.5 0 0014.5 1z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</div>
+					{:else if !file.isPublic && !file.client}
+						<!-- svelte-ignore a11y-no-static-element-interactions -->
+						<div
+							class="flex flex-row items-center cursor-pointer"
+							on:click={() => {
+								isPublic = true;
+								clientsOnly = false;
+								updateFile();
+							}}
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								class="w-5 h-5"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</div>
+					{/if}
+				{/if}
+
+				{#if loading}
+					<span class="btn"><span class="loading loading-spinner loading-xs"></span></span>
+				{:else}
+					<!-- svelte-ignore a11y-no-static-element-interactions -->
+					<span class="btn" on:click={viewFile}>View</span>
+				{/if}
+			</div>
 		</div>
 	</form>
 	<!-- svelte-ignore a11y-click-events-have-key-events -->
