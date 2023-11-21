14e0dff7e10e94c9d624b103bd004274f5c58de2
Joseph Wagner <68037585+wjoseph0@users.noreply.github.com>
2023-11-21 09:30:32 -0600

    Agent Dashboard: invite button when no journeys

--- src/lib/components/AgentDashboard.svelte
+++ src/lib/components/AgentDashboard.svelte
@@ -2,6 +2,7 @@
 	import { pb } from '$lib/pocketbase';
 	import { onMount, onDestroy } from 'svelte';
 	import Client from '$lib/components/Client.svelte';
+	import Invite from '$lib/components/Invite.svelte';
 
 	let journeys = [];
 	let files = [];
@@ -109,7 +110,12 @@
 					{/each}
 				{:else}
 					<tr>
-						<td class="text-xs prose"> No journeys to show </td>
+						<td class="prose flex items-center justify-center gap-2"> Invite your first client 
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+						  </svg>
+						  </td>
+						<td><Invite/></td>
 					</tr>
 				{/if}
 			</tbody>
