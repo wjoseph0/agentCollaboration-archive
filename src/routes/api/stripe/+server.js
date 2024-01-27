7a580c89409779c515dc758954d923b65b2164ee
Joseph Wagner <68037585+wjoseph0@users.noreply.github.com>
2024-01-27 13:39:02 -0600

    Update +server.js

--- src/routes/api/stripe/+server.js
+++ src/routes/api/stripe/+server.js
@@ -4,39 +4,44 @@ import { STRIPE_SECRET_KEY, STRIPE_WEBHOOK_SECRET } from '$env/static/private';
 import { env } from '$env/dynamic/private';
 import dayjs from 'dayjs';
 
-const pb = new Pocketbase('https://wjoseph0.cloud');
-await pb.admins.authWithPassword(env.POCKETBASE_ADMIN_EMAIL, env.POCKETBASE_ADMIN_PASSWORD);
-const stripe = new Stripe(STRIPE_SECRET_KEY);
-const endpointSecret = STRIPE_WEBHOOK_SECRET;
-
 export async function POST({ request }) {
+	const pb = new Pocketbase('https://wjoseph0.cloud');
+	const stripe = new Stripe(STRIPE_SECRET_KEY);
+	const endpointSecret = STRIPE_WEBHOOK_SECRET;
 	const payload = await request.text();
 	const sig = request.headers.get('stripe-signature');
-	let event;
 	try {
-		event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
-	} catch (err) {
-		return new Response(`${err}`, { status: 400 });
-	}
-	switch (event.type) {
-		case 'invoice.paid':
-			const expirationDate = dayjs.unix(event.data.object.lines.data[0].period.end);
-			const expirationDatePlusTwoDays = expirationDate.add(2, 'day').format();
-			const customer = await stripe.customers.retrieve(event.data.object.customer);
-			const user = await pb.collection('users').getFirstListItem(`email='${customer.email}'`);
-			try {
-				const subscriber = await pb
-					.collection('subscribers')
-					.getFirstListItem(`agent='${user.id}'`);
-				await pb
-					.collection('subscribers')
-					.update(subscriber.id, { expiration: expirationDatePlusTwoDays });
-			} catch (err) {
-				await pb
-					.collection('subscribers')
-					.create({ agent: user.id, expiration: expirationDatePlusTwoDays });
+		if (!sig) {
+			return new Response('', 400);
+		}
+		const event = await stripe.webhooks.constructEventAsync(payload, sig, endpointSecret);
+		switch (event.type) {
+			case 'invoice.paid': {
+				await pb.admins.authWithPassword(env.POCKETBASE_ADMIN_EMAIL, env.POCKETBASE_ADMIN_PASSWORD);
+				const expirationDate = dayjs.unix(event.data.object.lines.data[0].period.end);
+				const expirationDatePlusTwoDays = expirationDate.add(2, 'day').format();
+				const customer = await stripe.customers.retrieve(event.data.object.customer);
+				const user = await pb.collection('users').getFirstListItem(`email='${customer.email}'`);
+				try {
+					const subscriber = await pb
+						.collection('subscribers')
+						.getFirstListItem(`agent='${user.id}'`);
+					await pb
+						.collection('subscribers')
+						.update(subscriber.id, { expiration: expirationDatePlusTwoDays });
+				} catch (err) {
+					await pb
+						.collection('subscribers')
+						.create({ agent: user.id, expiration: expirationDatePlusTwoDays });
+				}
+				pb.authStore.clear();
+				break;
 			}
-			break;
+			default:
+				break;
+		}
+		return new Response('', 200);
+	} catch (err) {
+		return new Response(`${err}`, 400);
 	}
-	return new Response({}, { status: 200 });
 }
